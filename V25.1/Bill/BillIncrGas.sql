d660e90d2aee5a254c53e4aa1bf18206cf75decb
Jennifer Scheuerell <jennifer@sound-data.com>
2025-07-05 11:42:56 -0700

    Add full set of bill calculations

---
+++ V25.1/Bill/BillIncrGas.sql
@@ -0,0 +1,29 @@
+-- BillIncrGas
+-- 20250703
+
+SUM(
+	CASE
+		WHEN ISNULL(ft.FuelType,0) = '0'
+			AND (Thm1 * (ISNULL(RateG1, 0) + ISNULL(RateGFrac1,0)) + Thm2 * (ISNULL(RateGFrac2_1,0)+ISNULL(RateG2,0)+ISNULL(RateGFrac2_2,0))) < 0
+		THEN
+			ISNULL(
+				Qty *
+				IR *
+				RR *
+				( NTGRThm + @MEBens ) *
+				(
+					Thm1 *
+					(
+						ISNULL( RateG1, 0 ) +
+						ISNULL( RateGFrac1, 0 )
+					) +
+					Thm2 *
+					(
+						ISNULL( RateGFrac2_1, 0 ) +
+						ISNULL( RateG2, 0 ) +
+						ISNULL( RateGFrac2_2, 0 )
+					)
+				), 0
+			)
+		ELSE 0
+	END)
